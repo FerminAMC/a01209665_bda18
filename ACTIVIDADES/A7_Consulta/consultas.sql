76e6acaef23a504839d4c0b6e9b50794aa980936
FerminAMC <FerminAMC@github.com>
2018-02-07 15:46:29 -0600

    Actividad 7 terminada

---
+++ ACTIVIDADES/A7_Consulta/consultas.sql
@@ -0,0 +1,25 @@
+use classicmodels; 
+#explain select * from orderdetails;
+#show index from orderdetails;
+#describe orderdetails;
+explain
+select *
+from orderdetails 
+where quantityOrdered = 45;
+
+create index quantity_idx
+on orderdetails (quantityOrdered);
+
+show index from orderdetails;
+
+explain 
+select orders.*, orderdetails.*, products.*, productlines.* 
+from productlines 
+join products on products.productLine = productlines.productLine
+join orderdetails on orderdetails.productCode = products.productCode
+join orders on orders.orderNumber = orderdetails.orderNumber
+where customerNumber = 112;
+
+
+select * from orders
+
